97d1257c73c4e800d2554fb70df273c602fee582
吴焕楠 <wuhuannan@meizu.com>
2021-03-17 09:37:04 +0800

    学习

--- CustomViewMaterialEditText/app/src/main/java/com/hencoder/materialedittext/practice/MaterialEditText.kt
+++ CustomViewMaterialEditText/app/src/main/java/com/hencoder/materialedittext/practice/MaterialEditText.kt
@@ -7,7 +7,6 @@ import android.graphics.Paint
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatEditText
 import com.hencoder.materialedittext.R
 import com.hencoder.materialedittext.dp
@@ -16,45 +15,69 @@ private val TEXT_SIZE = 12.dp
 private val TEXT_MARGIN = 8.dp
 private val HORIZONTAL_OFFSET = 5.dp
 private val VERTICAL_OFFSET = 23.dp
-private val EXTRA_VERTICAL_OFFSET = 16.dp
 
 class MaterialEditText @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = R.attr.editTextStyle
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = R.attr.editTextStyle
 ) : AppCompatEditText(context, attrs, defStyleAttr) {
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        textSize = TEXT_SIZE
+    private val paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            textSize = TEXT_SIZE
+        }
     }
 
     // 用户辅助判断悬浮提示是否展示
     private var isFloatingLabelShowing = true
 
     // 用户控制浮提示动画
-    var floatingLabelFraction = 1f
+    var floatingLabelFraction = 1F
         set(value) {
             field = value
             invalidate()
         }
+    private val animator by lazy {
+        ObjectAnimator.ofFloat(this@MaterialEditText, "floatingLabelFraction", 1F, 0F)
+    }
+
+    // 控制是否展示浮提示
+    var useFloatingLabel = false
+        set(value) {
+            if (field != value) {
+                field = value
+                if (field) {
+                    // 增加额外的绘制区域
+                    setPadding(paddingLeft, (paddingTop + TEXT_SIZE + TEXT_MARGIN).toInt(), paddingRight, paddingBottom)
+                } else {
+                    setPadding(paddingLeft, (paddingTop - TEXT_SIZE - TEXT_MARGIN).toInt(), paddingRight, paddingBottom)
+                }
+            }
+        }
 
     init {
-        // 增加额外的绘制区域
-        setPadding(paddingLeft, (paddingTop + TEXT_SIZE + TEXT_MARGIN).toInt(), paddingRight, paddingBottom)
+        // 过滤属性
+        val typeArray = context.obtainStyledAttributes(attrs, R.styleable.MaterialEditText)
+        // 根据index获取属性值
+        useFloatingLabel = typeArray.getBoolean(R.styleable.MaterialEditText_useFloatingLabel, false)
+        // 回收资源
+        typeArray.recycle()
+
         addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!useFloatingLabel) {
+                    return
+                }
                 // 监听文字变化,进行动画
                 if (!s.isNullOrEmpty() && isFloatingLabelShowing) {
                     isFloatingLabelShowing = false
-                    Toast.makeText(context, "消失", Toast.LENGTH_SHORT).show()
-                    val animator = ObjectAnimator.ofFloat(this@MaterialEditText, "floatingLabelFraction", 1F, 0F)
+                    // 消失
                     animator.start()
                 } else if (s.isNullOrEmpty() && !isFloatingLabelShowing) {
                     isFloatingLabelShowing = true
-                    Toast.makeText(context, "出现", Toast.LENGTH_SHORT).show()
-                    val animator = ObjectAnimator.ofFloat(this@MaterialEditText, "floatingLabelFraction", 0F, 1F)
-                    animator.start()
+                    // 出现
+                    animator.reverse()
                 }
             }
 
@@ -65,9 +88,13 @@ class MaterialEditText @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        paint.alpha = (floatingLabelFraction * 0xff).toInt()
+        if (!useFloatingLabel) {
+            return
+        }
+        paint.alpha = (floatingLabelFraction * 0xFF).toInt()
         // 绘制文字
-        canvas.drawText(hint.toString(), HORIZONTAL_OFFSET, VERTICAL_OFFSET, paint)
+        val verticalOffset = VERTICAL_OFFSET + (1F - floatingLabelFraction) * 16.dp
+        canvas.drawText(hint.toString(), HORIZONTAL_OFFSET, verticalOffset, paint)
     }
 
 }
