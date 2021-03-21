87ed4aaf0bb2fd6187ddd0125eb2457e40768148
吴焕楠 <wuhuannan@meizu.com>
2021-03-21 12:40:47 +0800

    学习

--- CustomViewLayoutSize/app/src/main/java/com/hencoder/layoutsize/view/CircleView.kt
+++ CustomViewLayoutSize/app/src/main/java/com/hencoder/layoutsize/view/CircleView.kt
@@ -8,21 +8,26 @@ import android.view.View
 import com.hencoder.layoutsize.dp
 
 private val RADIUS = 100.dp
-private val PADDING = 100.dp
+private val PADDING = 20.dp
 
+/**
+ * 完全⾃定义 View 的尺⼨
+ */
 class CircleView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
   private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+    // 计算出⾃⼰的尺⼨
     val size = ((PADDING + RADIUS) * 2).toInt()
+    // ⽤ resolveSize() 或者 resolveSizeAndState() 修正结果
     val width = resolveSize(size, widthMeasureSpec)
     val height = resolveSize(size, heightMeasureSpec)
+    // 使⽤ setMeasuredDimension(width, height) 保存结果
     setMeasuredDimension(width, height)
   }
 
   override fun onDraw(canvas: Canvas) {
     super.onDraw(canvas)
-
     canvas.drawCircle(PADDING + RADIUS, PADDING + RADIUS, RADIUS, paint)
   }
 }
--- CustomViewLayoutSize/app/src/main/java/com/hencoder/layoutsize/view/SquareImageView.kt
+++ CustomViewLayoutSize/app/src/main/java/com/hencoder/layoutsize/view/SquareImageView.kt
@@ -5,12 +5,17 @@ import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import kotlin.math.min
 
+/**
+ * 简单改写已有 View 的尺⼨
+ * 例子:方形ImageView,以短边为标准
+ */
 class SquareImageView(context: Context?, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+    // ⽤ getMeasuredWidth() 和 getMeasuredHeight() 获取到测量出的尺⼨
     super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-
+    // 计算出最终要的尺⼨
     val size = min(measuredWidth, measuredHeight)
-
+    // ⽤ setMeasuredDimension(width, height) 把结果保存
     setMeasuredDimension(size, size)
   }
 }
