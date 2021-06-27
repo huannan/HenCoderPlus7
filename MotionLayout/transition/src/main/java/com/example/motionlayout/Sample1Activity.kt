09b22e90e7f0e74237141cea35e1ac788732f4f1
吴焕楠 <wuhuannan@meizu.com>
2021-06-27 21:32:59 +0800

    学习

--- MotionLayout/transition/src/main/java/com/example/motionlayout/Sample1Activity.kt
+++ MotionLayout/transition/src/main/java/com/example/motionlayout/Sample1Activity.kt
@@ -3,10 +3,10 @@ package com.example.motionlayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SampleActivity : AppCompatActivity() {
+class Sample1Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample)
+        setContentView(R.layout.activity_sample1)
     }
 }
